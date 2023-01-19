1258318cc8b0780ed69cffeb64edbf69e9e9c821
gustavodemate <dema_dev@hotmail.com>
2023-01-18 21:37:38 -0300

    ci: Add docs workflow

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -1,4 +1,4 @@
-import { colors, fonts, fontSizes, fontWeights, lineHeights, radii, space } from "@ignite-ui/tokens";
+import { colors, fonts, fontSizes, fontWeights, lineHeights, radii, space } from "@demate-ignite-ui/tokens";
 import { createStitches, defaultThemeMap, DefaultThemeMap } from "@stitches/react";
 
 export const {
